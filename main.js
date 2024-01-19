93e6b5b5d83986d718963d95acf82b9c113792c0
delusional51 <BenjaminZanelli51@gmail.com>
2024-01-19 13:13:44 -0500

    got it work

--- main.js
+++ main.js
@@ -4,34 +4,45 @@ use this api  */
 const DOMSelectors = {
     form: document.querySelector("#form"),
     itemname: document.querySelector("#item-name"),
+    spchprt: document.querySelector("#part-of-spch"),
     parent1: document.querySelector(".container"),
-
+    input: document.querySelector(".input")
 }
 
 //4387 up n down 1 to get teams
-const url = 'https://americanfootballapi.p.rapidapi.com/api/american-football/team/4386/players';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '0aac7e6b1dmshe54e5f3ba0d7c82p1e541ejsn8479615d9fd8',
-		'X-RapidAPI-Host': 'americanfootballapi.p.rapidapi.com'
-	}
-};
+async function getData(x,y){
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${x}`;
+    try {
+    	const response = await fetch(url);
+	    const result = await response.json();
+        console.log(response)
+	    console.log(result[0]);
+        let thing = result[0]
+        DOMSelectors.parent1.insertAdjacentHTML(
+            "beforeend",
+            `<div class=${thing.word}>
+            <h2>${thing.word.toUpperCase()}</h2>
+            <h3>${thing.meanings[0].partOfSpeech.toUpperCase()}</h3>
+            <h4>${thing.meanings[0].definitions[0].definition}</h4>
+            </div>`
+        )
+        
+    } 
+    catch (error) {
+	    console.error(error);
+    }
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
 }
-/* function insert(){
+function insert(){
+    DOMSelectors.itemname.addEventListener('click', function(){
+        DOMSelectors.itemname.value=""
+    })
     DOMSelectors.form.addEventListener('submit',function(event){
         event.preventDefault()
         let x = DOMSelectors.itemname.value.toLowerCase()
-        let y = x.replaceAll(' ','+')
-        console.log(y)
-        getData(y)
+        let z = DOMSelectors.spchprt.value.toLowerCase()
+        console.log(x,z)
+        getData(x,z)
     })
 }
-insert() */
+insert()
